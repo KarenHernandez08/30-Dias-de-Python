d66a78140bd6b0f326ac14d97438197903608a5e
KarenHernadez08 <ada_hack109@hackademy.mx>
2022-03-23 13:46:48 -0600

    Día 1
    Ejercicios del día 1 sobre  aprender python en 30 días

---
+++ Dia1/Ejercicios.py
@@ -0,0 +1,50 @@
+"""
+Ejercicio 1
+Realizar las siguientes operaciónes, con los operadores 3 y 4
+suma (+)
+sustracción(-)
+multiplicación(*)
+módulo(%)
+división(/)
+exponencial(**)
+operador de división de piso(//)
+"""
+print("---------Este es el ejercico 1---------")
+suma=3+4
+print("Esta es la operación de la suma: ",suma)
+resta= 3-4
+print("Esta es la operación de la resta: ",resta)
+multiplicacion=3*4
+print("Esta es la operación de la multiplicación: ",multiplicacion)
+modulo=3%4
+print("Esta es la operación del restante: ",modulo)
+division=3/4
+print("Esta es la operación de la división: ",division)
+exponencial=3**4
+print("Esta es la operación de la exponencial: ",exponencial)
+divipiso=3//4
+print("Esta es la operación de la división de piso: ", divipiso)
+
+
+
+
+"""
+Ejercicio 2
+Escribe cadenas con:
+Nombre, apellido, el país
+
+"""
+print("---------Este es el ejercico 2---------")
+lista=['Karen', 'Hernández','Mexico' ]
+print(lista)
+
+"""
+Ejercicio 3
+Encuentra una distancia euclidiana entre (2,3) y (10,8)
+"""
+print("---------Este es el Ejercico 3---------")
+from math import dist
+a=(2,3)
+b=(10,8)
+print(" La distancia euclidiana entre (2,3) y (10,8) es:  ",dist(a,b))
+
